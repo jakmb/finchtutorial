3fb9203ccb4d706b04afb0f80503dd858e5de102
Jonathan Butturini <jakmb@umich.edu>
2016-07-23 16:33:43 -0400

    Example program commit

--- testFinch.cpp
+++ testFinch.cpp
@@ -10,6 +10,8 @@ int main(){
     }//if
     unsigned int rgb[] = {255, 0, 0};
     myFinch.setLED(rgb[0], rgb[1], rgb[2]);
+    myFinch.setMotors(100, 100, 3000);
+    myFinch.setMotors(0,0);
     return 0;
 }//main()
 
